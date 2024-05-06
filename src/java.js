4b38b8fb9b3b1d37ab885fe3c553fe08e3e04007
Danieldd96 <danieldelgado.dd270u@gmail.com>
2024-05-06 16:05:08 -0600

    lunes

---
+++ src/java.js
@@ -0,0 +1,28 @@
+const LoginBtnnLink = document.querySelector('.loginBtn');
+const RegisterBtnnLink = document.querySelector('.registerBtn');
+const wrapper = document.querySelector('.wrapper');
+const menu = document.getElementById("menu")
+const menu2 = document.getElementById("menu2")
+const ocultarMenu = document.getElementById("ocultarMenu")
+LoginBtnnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+    menu.classList.remove("hidden")
+    menu2.classList.add("hidden")
+});
+RegisterBtnnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+    menu.classList.add("hidden")
+    menu2.classList.remove("hidden")
+});
+
+
+
+function Login() {
+    const usu = document.getElementById('user').value;
+    const pass = document.getElementById('pass').value;
+    console.log(usu, pass);
+    if(usu == 'Danieluser' && pass == '123456'){
+        window.location.href = 'galeria.html'
+    }
+}
+
