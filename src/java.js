a454a2575975e59318d8c38cb8848aa3e6ca5f8e
Danieldd96 <danieldelgado.dd270u@gmail.com>
2024-05-10 16:02:13 -0600

    viernes

--- src/java.js
+++ src/java.js
@@ -16,47 +16,38 @@ RegisterBtnnLink.addEventListener('click', () => {
     menu2.classList.remove("hidden")
 });
 
-const usuario = document.getElementById("name");
-const email = document.getElementById("email");
-const password = document.getElementById("password")
-let listaN =[]
-function Datos() {
 
 
 
+const registerForm = document.querySelector('#registerForm')
+registerForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
 
-
-
-
-
-
-
-
-
-
-
-
-
-    localStorage.setItem('user-name', usuario.value)
-    localStorage.setItem('email', email.value)
-    localStorage.setItem('password', password.value)
-    
-    
-}
-
-
-function Login() {
-    const usu = document.getElementById('user').value;
-    const pass = document.getElementById('pass').value;
-    var nombre = localStorage.getItem("user-name");
-    var email = localStorage.getItem("email");
-    var contra = localStorage.getItem("password");
-
-
-    console.log(usu, pass);
-    if(usu === nombre && pass === contra){
-        window.location.href = 'galeria.html'
-        alert("Inicio de sesion Exitoso")
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const UserRegister = Users.find(user => user.email === email)
+    if (UserRegister) {
+        return alert('Usuario ya registrado!')
+        
     }
-}
+   Users.push({name: name, email: email, password: password})
+   localStorage.setItem('users', JSON.stringify(Users))
+   alert('Registro completo')
+});
 
+const loginForm = document.querySelector('#loginForm')
+loginForm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    const name = document.querySelector('#user').value
+    const password = document.querySelector('#pass').value
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const validUser = Users.find(user => user.name === name && user.password === password)
+    if (!validUser) {
+        return alert("usuario y/o contra incorrecta!")
+        
+    }
+    alert("Bienvenido")
+    window.location.href = "/src/galeria.html"
+})
